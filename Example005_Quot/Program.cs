1148103f6b0f2bcf1534873b24e43b787e565a03
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-08-29 13:16:19 +0300

    If_Else

--- Example005_Quot/Program.cs
+++ Example005_Quot/Program.cs
@@ -1,4 +1,4 @@
-﻿double first = new Random().Next(4,8);
+﻿double first = new Random().Next(4,8);//4,5,6,7 but not 8
 double second = 1;
 double third = first / second;
 Console.WriteLine(third);
---
+++ Example006_CheckUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВЕДИТЕ ВАШЕ ИМЯ");
+string username = Console.ReadLine();
+if(username.ToLower() == "михаил")
+{
+    Console.WriteLine("КАК Я РАД ВАМ, МИХАИЛ!");    
+}   
+else
+{
+    Console.Write("ЗДРАВСТВУЙТЕ ");
+    Console.Write(username);
+}
