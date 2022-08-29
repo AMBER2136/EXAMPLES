2aee58b7dcb762b67c992af9b3925daaee3e3f93
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-08-29 07:50:15 +0300

    003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("ВВЕДИТЕ ВАШЕ ИМЯ");
+string name = Console.ReadLine();
+Console.Write("ЗДРАВСТВУЙТЕ, ");
+Console.Write(name);
+Console.WriteLine("ВВЕДИТЕ ПЕРВОЕ ЧИСЛО");
+double first = Console.ReadLine();
+Console.WriteLine("ВВЕДИТЕ ВТОРОЕ ЧИСЛО");
+double second = Console.ReadLine();
+Console.WriteLine(first + second);
