8d301b06047ebbb416f4b8b57821c65256c08464
AMBER2136 <mikhail-grishkov@yandex.ru>
2022-08-30 12:37:39 +0300

    count

---
+++ Example009_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿double distance = 10000;
+double speedFirst = 2;
+double speedSecond = 1;
+double speedDog = 5;
+double time;
+int count = 0;
+string friend = "first";
+
+
+while(distance < 10)
+{
+    if (friend == "first")
+    {
+        time = distance / (speedDog + speedSecond);
+        distance = distance - time * (speedFirst + speedSecond);
+        friend = "second";
+        count=count+1;
+    }
+    if (friend == "second")
+    {
+        time = distance / (speedDog + speedFirst);
+        distance = distance - time * (speedFirst + speedSecond);
+        friend = "first";
+        count=count+1;
+    }
+count=count+1;
+}
+Console.WriteLine(count);
+
